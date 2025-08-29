9e596cc785afa72bcbd84412e436dcac868ecc81
Youssef Elsabawy <yelsabawyy@gmail.com>
2025-08-29 08:25:45 +0300

    add logo

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,9 +1,12 @@
+import Image from "next/image";
 import React from "react";
 
 export default function Header() {
   return (
     <div className="flex items-center justify-between p-6 bg-[#1A1A5D]">
-      <div className="text-2xl md:text-4xl font-bold text-white">Youssef Elsabawy</div>
+      <div className="text-2xl md:text-4xl font-bold text-white">
+        <Image width={150} height={0} src="/Sentiomail.png" alt="Sentiomail" />
+      </div>
     </div>
   );
 }
